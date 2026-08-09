47cfa9e19ed46200227e9aaabff51b3c72cc1b3e
ynarawade <yadnesh.narawade22@gmail.com>
2026-08-09 23:12:38 +0530

    added schema for users & profiles.

---
+++ packages/db/prisma/migrations/20260809174159_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateEnum
+CREATE TYPE "AuthMethod" AS ENUM ('email', 'google');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "email_verified" BOOLEAN NOT NULL,
+    "aut_method" "AuthMethod" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "user_id" TEXT NOT NULL,
+    "avatar_url" TEXT,
+    "first_name" TEXT,
+    "last_name" TEXT,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
